b8773d45cb5a14f44b808058ea930a5dd40346bc
Nathan Jones <njones93531@gmail.com>
2025-05-09 10:34:47 -0400

    Fixed timeout bug in ubuntu

--- benchmarks/strategied/common/numa_exp.sh
+++ benchmarks/strategied/common/numa_exp.sh
@@ -5,7 +5,7 @@ out=$3
 data=${logbase}_numa_pref.data
 
 echo $out >> $data
-if timeout $TIMEOUT time 2>> $data numactl --cpunodebind=0 --preferred=1 $cmd >> $data; then
+if timeout $TIMEOUT $(numactl --cpunodebind=0 --preferred=1 $cmd >> $data); then
 	echo "Command did not time out\n" >> $data
 else
 	echo "Command timed out after $TIMEOUT seconds\n" >> $data
