4bb511833db3cb0c0f6d4287dd38756869111874
Nathan Jones <njones93531@gmail.com>
2025-08-23 12:51:35 -0400

    Naming consistency

--- scripts/paper-figs-subset.sh
+++ scripts/paper-figs-subset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TIMEOUT=1200
-./genfaults.sh -s -t $TIMEOUT 	#Estimated  3 hours
+./generate-faults.sh -s -t $TIMEOUT 	#Estimated  3 hours
 ./evaluate-perf.sh -s -t $TIMEOUT  #Estimated  6 hours
 ./process-faults.sh -s -t $TIMEOUT #Estimated <1 hours
 ./process-perf.sh -s -t $TIMEOUT   #Estimated <1 hours
