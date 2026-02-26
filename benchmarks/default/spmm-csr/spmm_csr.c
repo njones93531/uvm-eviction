79e53b0544be85ab5be03b3ea855af21d3a20716
Nicholas Cassarino <ncassari@charlotte.edu>
2026-02-26 11:01:53 -0500

    fully running automatic path; ungeneralized; hardcoded aspects

--- benchmarks/default/spmm-csr/spmm_csr.c
+++ benchmarks/default/spmm-csr/spmm_csr.c
@@ -139,8 +139,7 @@ int main(int argc, char *argv[]) {
     float beta            = 0.0f;
     //--------------------------------------------------------------------------
     // Device memory management
-    CHECK_CUDA( cudaMallocManaged((void**) &hA_csrOffsets,
-                           (A_num_rows + 1) * sizeof(int), cudaMemAttachGlobal))
+    CHECK_CUDA( cudaMallocManaged((void**) &hA_csrOffsets, (A_num_rows + 1) * sizeof(int), cudaMemAttachGlobal))
     CHECK_CUDA( cudaMallocManaged((void**) &hA_columns, A_nnz * sizeof(int), cudaMemAttachGlobal))
     CHECK_CUDA( cudaMallocManaged((void**) &hA_values,  A_nnz * sizeof(float), cudaMemAttachGlobal))
     CHECK_CUDA( cudaMallocManaged((void**) &hB,         B_size * sizeof(float), cudaMemAttachGlobal))
