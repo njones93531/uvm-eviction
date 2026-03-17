58aae5c97ebfef8a6b5584c3840a5f9c9a306bad
Nicholas Cassarino <ncassari@uncc.edu>
2026-03-17 19:06:50 +0000

    Added handling for numactl; when there is just 1 NUMA node we use 0, when the system has 2+ nodes we use 1

--- benchmarks/strategied/common/numa_exp.sh
+++ benchmarks/strategied/common/numa_exp.sh
@@ -4,11 +4,25 @@ cmd=$2
 out=$3
 data=${logbase}_numa_pref.data
 
+NUMA_COUNT=$(numactl --hardware 2>/dev/null | awk '/available:/ {print $2}')
+
+# fallback if parsing fails
+if ! [[ "$NUMA_COUNT" =~ ^[0-9]+$ ]]; then
+  NUMA_COUNT=1
+fi
+
+MAX_NODE=$((NUMA_COUNT - 1))
+
+# requested node (you were using 1)
+REQ_NODE=1
+
+# clamp to valid range
+NUMA_NODE=$(( REQ_NODE > MAX_NODE ? MAX_NODE : REQ_NODE ))
 
 use_timeout=1
 if [ $use_timeout -eq 1 ]; then
   echo $out >> $data
-  timeout "$TIMEOUT" numactl --cpunodebind=0 --preferred=1 bash -c "$cmd" >> "$data"
+  timeout "$TIMEOUT" numactl --cpunodebind=0 --preferred=$NUMA_NODE bash -c "$cmd" >> "$data"
   if [ $? -eq 0 ]; then
     echo "Command did not time out within $TIMEOUT seconds\n" >> $data
   else
@@ -16,6 +30,5 @@ if [ $use_timeout -eq 1 ]; then
   fi
 else
   echo $out >> $data
-  numactl --cpunodebind=0 --preferred=1 bash -c "$cmd" >> "$data"
+  numactl --cpunodebind=0 --preferred=$NUMA_NODE bash -c "$cmd" >> "$data"
 fi
-
